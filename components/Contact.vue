97db8ddcb31a56e518c536f132e55070def45c89
Arif Mohammad <arif.iwamoto@gmail.com>
2021-02-25 14:24:57 +0900

    change

--- components/Contact.vue
+++ components/Contact.vue
@@ -8,7 +8,8 @@
         netlify
         name="contactus"
         data-netlify="true"
-        netlify-honeypot="bot-field"
+        data-netlify-honeypot="bot-field"
+        data-netlify-recaptcha="true"
       >
         <input type="hidden" name="form-name" value="contactus" />
         <h3 style="text-align: center">お問い合わせ</h3>
